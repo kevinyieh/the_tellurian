ab4b9fc98440a04ce0283fb9cb8f07067cfaee85
“Becca” <r.e.burten@gmail.com>
2020-10-05 07:40:39 -0700

    Include alien icon with credits

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,14 +1,15 @@
 import React from 'react';
-// import { AuthRoute, ProtectedRoute } from '../util/route_util';
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import { Switch } from 'react-router-dom';
-// import NavBarContainer from './nav/navbar_container';
+import Splash from './splash/splash';
 import { Route } from "react-router-dom";
 import MainPage from './main/main_page';
 
 const App = () => (
   <div>
     <Switch>
-        <Route exact path="/main" component={MainPage} />
+      <Route exact path="/" component={Splash} />
+      <ProtectedRoute exact path="/main" component={MainPage} />
     </Switch>
   </div>
 );
---
+++ frontend/src/components/splash/splash.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Splash extends React.Component {
+
+    render() {
+        return (
+            
+            <div>
+                Icons made by < a href="https://www.flaticon.com/authors/icongeek26" title="Icongeek26" > Icongeek26</a > from < a href="https://www.flaticon.com/" title="Flaticon" > www.flaticon.com</a>
+                Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
+            </div>
+        )
+    }
+}
+
+export default Splash;
+
