a63d464f5f3695f89e88a2b25b42478bf9490601
“Becca” <r.e.burten@gmail.com>
2020-10-05 08:31:54 -0700

    Template for Splash created, title image and byline

--- frontend/src/components/splash/splash.js
+++ frontend/src/components/splash/splash.js
@@ -4,10 +4,18 @@ class Splash extends React.Component {
 
     render() {
         return (
-            
+
             <div>
-                Icons made by < a href="https://www.flaticon.com/authors/icongeek26" title="Icongeek26" > Icongeek26</a > from < a href="https://www.flaticon.com/" title="Flaticon" > www.flaticon.com</a>
-                Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
+                <h1 className="logo">The Tellurian</h1>
+                <img alt="tellurianLogo" src={require('../../images/alien.png')} />
+                <h2>Find all the news you need on the world, on the world.</h2>
+
+                <button>Enter Here</button>
+
+                <div className="credits">
+                    Icons made by < a href="https://www.flaticon.com/authors/icongeek26" title="Icongeek26" > Icongeek26</a > from < a href="https://www.flaticon.com/" title="Flaticon" > www.flaticon.com</a>
+                    Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
+                </div>
             </div>
         )
     }
