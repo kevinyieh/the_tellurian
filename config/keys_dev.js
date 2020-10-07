09c683ae2d1576ce901fb17320fbb76bc7de23e5
WilsonLi <ligokdoon@gmail.com>
2020-10-07 11:03:54 -0400

    search route

---
+++ config/keys_dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: "mongodb+srv://admin:dz1BAAbx5XRTxR1o@cluster0.bzznm.mongodb.net/db?retryWrites=true&w=majority",
+    secretOrKey: "fdklj43957fghkse4%F&Gnhof@UYFD^j4bklSDFS"
+}
