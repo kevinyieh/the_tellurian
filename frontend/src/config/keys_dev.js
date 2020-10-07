4fd9e95f9283fb9f357d2201e5314fe830488131
hernald <herndonald@gmail.com>
2020-10-07 16:30:25 -0400

    Config cleanup

---
+++ frontend/src/config/keys_dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://admin:dz1BAAbx5XRTxR1o@cluster0.bzznm.mongodb.net/db?retryWrites=true&w=majority",
+  secretOrKey: "fdklj43957fghkse4%F&Gnhof@UYFD^j4bklSDFS",
+  catcherKey: "5691491458mshdf755bcd0f358c9p1eebcdjsn36e0a26ce65c",
+  nytKey: "7cGqbWtlY4IddrzXGqbMpaTUAbLKkgrX"
+};
