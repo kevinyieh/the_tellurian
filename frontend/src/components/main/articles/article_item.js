f7df9c114ae7c16f609245f4aa3fbddec790e3d2
“Becca” <r.e.burten@gmail.com>
2020-10-08 11:38:01 -0700

    kevin to add tabs

--- frontend/src/components/main/articles/article_item.js
+++ frontend/src/components/main/articles/article_item.js
@@ -1,20 +1,59 @@
 import React from 'react';
 
 class ArticleItem extends React.Component {
+
+    constructor(props) {
+      super(props);
+      this.state = { collapsed: true };
+      this.toggleContent = this.toggleContent.bind(this);
+      this.imageRender = this.imageRender.bind(this);
+    }
+
+    toggleContent(e) {
+      e.preventDefault();
+      debugger;
+      this.setState({ collapsed: !this.state.collapsed });
+    }
+
+    imageRender(articleURL) {
+      if (!articleURL) return null;
+      return <img src={require(articleURL)} alt="article" />
+    }
+
     render() {
         const { article } = this.props;
+        debugger;
         return (
           <div className="article-item">
-            <img src={article.imageURL} alt="article"/>
             <div className="article-text">
-                <a className="headline" href={article.articleURL}>
-                  {article.headline}
-                </a>
-                <p>{article.date}</p>
-                <i className="fas fa-chevron-down"></i>;
-              <div className="article-content">
-                <p>{article.body}</p>
+              <div className="flex-head">
+                <i
+                  className={`fas fa-chevron-down ${
+                    this.state.collapsed ? "" : "rotate"
+                  }`}
+                  onClick={this.toggleContent}
+                ></i>
+                <h1 className="headline">{article.headline}</h1>
+                {this.imageRender(article.imageURL)}
               </div>
+              <p>{article.date}</p>
+              <p
+                className={`article-content ${
+                  this.state.collapsed ? "" : "display"
+                }`}
+              >
+                {article.body}
+                <br></br>
+                <a
+                  className="article-link"
+                  href={article.articleURL}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Read More
+                </a>
+              </p>
+              {/* </div> */}
             </div>
           </div>
         );
